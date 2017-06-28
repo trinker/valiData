fcd166e90d25e7c17d99ff19b806c2d36be465d0
Tyler Rinker <trinker@campuslabs.com>
2017-06-28 10:35:08 -0400

    bug introduced on last commit nuked....didn't reassign dat[[x]]] to col

--- R/vc_cipcode.R
+++ R/vc_cipcode.R
@@ -18,6 +18,7 @@ vc_cipcode <- function(data, x, ...){
     ## select the column & replace missing with NA
     ## col <- sub_out_missing(data[[x]])  ## remove missing substitution as
     ##                                       we do not allow NULL etc on 2017-06-28
+    col <- data[[x]]
 
     ## record missing (NA)
     is_na <- is.na(col)
