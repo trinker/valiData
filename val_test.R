565f1d19e1ddf5394f66cd1f9b190f16cb129a55
Tyler Rinker <trinker@campuslabs.com>
2020-01-28 10:50:58 -0500

    added some new ways to do testing

--- val_test.R
+++ val_test.R
@@ -7,11 +7,11 @@ clDev::source_R_folder()
 
 
 
-inst <- 'poo'
-file <- sprintf("C:\\Users\\trinker\\OneDrive - Campus Labs LLC\\Desktop\\Data_Validation\\TestCore\\%s/Accounts/AccountImports/Account.csv", inst)
-file.exists(file)
-loc <- sprintf("C:\\Users\\trinker\\OneDrive - Campus Labs LLC\\Desktop\\Data_Validation\\TestCore/%s", inst)
-map_loc <- 'C:\\Users\\trinker\\OneDrive - Campus Labs LLC\\Desktop\\Data_Validation\\TestCore\\bin/Core_Data_Dictionary_DS_longforms.xlsx'
+inst <- 'bhc'
+file <- dir(sprintf("C:/Users/trinker/OneDrive - Campus Labs LLC/Desktop/Data_Validation/TestCore/%s/Accounts/AccountImports", inst), pattern = '/.csv$', full.names = TRUE)
+file ## Make sur there's just one
+loc <- sprintf("C:/Users/trinker/OneDrive - Campus Labs LLC/Desktop/Data_Validation/TestCore/%s", inst)
+map_loc <- 'C:/Users/trinker/OneDrive - Campus Labs LLC/Desktop/Data_Validation/TestCore/bin/Core_Data_Dictionary_DS_longforms.xlsx'
 map <- import_map(map_loc)
 
 valiData(loc, map)
@@ -25,6 +25,8 @@ pp7 <- compare_column(
         parent.column='PersonIdentifier',
         parent='AccountImports',
         child = c('Enrollment', 'FacultyRemoval', 'Instructor', 'FacultyImport', 'StudentImport'),
-        ignore.case = TRUE
-
+        ignore.case = TRUE,
+        ignore.element.case = TRUE
     )
+
+
